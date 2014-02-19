15da034b186719ddf1fbc83ddd8a2cc5cd990207
zenglong <admin@zengl.com>
2014-02-19 14:48:40 +0800

    修复terminated by signal (11)异常中止的BUG
    通过使用zengl语言的v1.4.1版本,来修复该BUG,该Bug的详情请参考zengl_language项目

--- android/jni/src/Android.mk
+++ android/jni/src/Android.mk
@@ -7,9 +7,12 @@ LOCAL_MODULE    := zengl
 LOCAL_SRC_FILES := $(ZENGL_PATH)/zengl_main.c  $(ZENGL_PATH)/zengl_parser.c  $(ZENGL_PATH)/zengl_symbol.c  $(ZENGL_PATH)/zengl_locals.c  $(ZENGL_PATH)/zengl_assemble.c  $(ZENGL_PATH)/zengl_ld.c  $(ZENGL_PATH)/zenglrun_main.c  $(ZENGL_PATH)/zenglrun_func.c  $(ZENGL_PATH)/zenglApi.c  $(ZENGL_PATH)/zenglApi_BltModFuns.c $(ZENGL_PATH)/zenglDebug.c
 
 LOCAL_CFLAGS    := -DZL_LANG_EN_WITH_CH -DZL_EXP_OS_IN_ARM_GCC
-LOCAL_CFLAGS += -DHAVE_SYS_UIO_H
+#LOCAL_CFLAGS += -DHAVE_SYS_UIO_H
+#LOCAL_CFLAGS    += -g
+#LOCAL_CFLAGS    += -ggdb
+#LOCAL_CFLAGS    += -O1
 
-LOCAL_LDLIBS := -llog
+#LOCAL_LDLIBS := -llog
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -24,6 +27,10 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/../SDL_image
 # Add your application source files here...
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.cpp \
 	testgles.c jni_glue.cpp mySDL_Lib.c
+
+#LOCAL_CFLAGS    += -g
+#LOCAL_CFLAGS    += -ggdb
+#LOCAL_CFLAGS    += -O1
 
 LOCAL_STATIC_LIBRARIES := zengl
 LOCAL_SHARED_LIBRARIES := SDL SDL_image SDL_ttf
